0370dabb15ca536df80ea9635f61d85c94902f05
Aaron Friel <mayreply@aaronfriel.com>
2024-01-25 11:00:01 -0800

    Update index.ts

--- friel-test-4/index.ts
+++ friel-test-4/index.ts
@@ -10,9 +10,3 @@ const bucket = new aws.s3.Bucket("my-bucket", {
 aws.getCallerIdentityOutput().apply(x => {
   console.log(`hello ${x.accountId}`);
 });
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
-
-// Additional: Export the bucket endpoint, if you intend to use it in some application or for reference.
-export const bucketEndpoint = pulumi.interpolate`http://${bucket.bucketRegionalDomainName}`;
