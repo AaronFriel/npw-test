0d03fa1fa61031641d187a328b58d71924994bd3
AaronFriel <788800+AaronFriel@users.noreply.github.com>
2024-01-25 18:52:38 +0000

    [Pulumi] automated commit for new project trustworthy-ruby-red-panda
    Created with app.pulumi.com

---
+++ friel-test-4/index.ts
@@ -0,0 +1,14 @@
+import * as aws from "@pulumi/aws";
+
+const bucket = new aws.s3.Bucket("my-bucket", {
+    // The name of the bucket. This name is globally unique, and the namespace is shared by all AWS accounts.
+    bucket: "my-unique-bucket-name", 
+    // By default, all S3 buckets are private. Set the ACL to 'public-read' if you want the bucket to be publicly readable.
+    acl: "private", 
+});
+
+// Export the name of the bucket
+export const bucketName = bucket.id;
+
+// Additional: Export the bucket endpoint, if you intend to use it in some application or for reference.
+export const bucketEndpoint = pulumi.interpolate`http://${bucket.bucketRegionalDomainName}`;
